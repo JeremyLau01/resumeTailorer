d22c1ea448e5c3e599ae05cdeb996025958ce17e
Jeremy Lau <52184467+JeremyLau01@users.noreply.github.com>
2024-03-15 10:33:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -519,3 +519,6 @@ def greet():
   flash(text)
   
   return render_template("index.html", data=text)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
